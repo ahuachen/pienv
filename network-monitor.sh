8e5761e51d96e504cbb92ce5c177b7a0f77e25a0
ahua <ahua.chen@gmail.com>
2015-08-12 14:59:44 +0800

    add mjpg-stream for pi2

--- network-monitor.sh
+++ network-monitor.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+DATE=`date +%Y%m%d`
 if /sbin/ifconfig wlan0 | grep -q "inet addr:" ; then
-      echo `date` "Network OKOKOKOKOKOKOK!" | tee -a /tmp/network-moniter.log
+      echo `date` "Network OKOKOKOKOKOKOK!" | tee -a /tmp/network-moniter_$DATE.log
       exit
 else
-      echo `date` "Network connection down! Attempting reconnection." | tee -a /tmp/network-moniter.log
-      /sbin/ifconfig wlan0 | tee -a /tmp/network-moniter.log
+      echo `date` "Network connection down! Attempting reconnection." | tee -a /tmp/network-moniter_$DATE.log
+      /sbin/ifconfig wlan0 | tee -a /tmp/network-moniter_$DATE.log
       /sbin/ifup --force wlan0
+      if /sbin/ifconfig wlan0 | grep -q "inet addr:" ; then
+	   echo `date` "Network OKOKOKOKOKOKOK!" | tee -a /tmp/network-moniter_$DATE.log
+      else
+	   echo `date` "Network up error!"  | tee -a /tmp/network-moniter_$DATE.log
+      fi
+
+
 fi
 
 #crontab -u root -e
---
+++ webcam.sh
@@ -0,0 +1,2 @@
+fswebcam -v -r 640x480 --no-banner ./foo.jpg
+echo look ./foo.jpg
